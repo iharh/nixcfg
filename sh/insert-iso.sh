a4644dac276d4648ed3e8630f982affede601d69
Ihar Hancharenka <ihar.hancharenka@inspirebrands.com>
2024-01-01 13:11:51 +0300

    m

--- sh/insert-iso.sh
+++ sh/insert-iso.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 NIXOS_ISO=`find $HOME/Downloads/dist/nixos -name "nixos-*.iso"`
-# --source $NIXOS_ISO \
 virsh change-media \
   --domain nixos \
-  --path $NIXOS_ISO \
+  --path sda \
+  --source $NIXOS_ISO \
   --insert \
   --config
