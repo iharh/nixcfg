e5fd8f267a3920708153d48b3c5b4804fc2702e8
Ihar Hancharenka <ihar.hancharenka@inspirebrands.com>
2024-01-05 22:45:15 +0300

    m

--- hw/kvm-x86_64-linux.nix
+++ hw/kvm-x86_64-linux.nix
@@ -24,7 +24,7 @@
       grub = {
         enable = true;
         # "uuid", "label", "provided"
-        fsIdentifier = "label";
+        fsIdentifier = "uuid";
         ## "nodev" for efi only 
         ## /dev/vda
         device = "nodev";
