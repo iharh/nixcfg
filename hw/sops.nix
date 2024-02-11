16564b0e400cc92b0100825f02b5ceb4d1565182
Ihar Hancharenka <ihar.hancharenka@inspirebrands.com>
2024-02-11 15:09:41 +0300

    m

--- hw/sops.nix
+++ hw/sops.nix
@@ -13,7 +13,7 @@
         owner = config.users.users.iharh.name; 
       };
       "myservice/my_subdir/my_secret" = {
-        owner = "config.users.users.${myuser}.name";
+        owner = config.users.users.${myuser}.name;
       };
     };
   };
