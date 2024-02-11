a9fddf811f61d38f67fe48e5904c2ff88fc03460
Ihar Hancharenka <ihar.hancharenka@inspirebrands.com>
2024-02-11 15:11:44 +0300

    m

--- hm/home.nix
+++ hm/home.nix
@@ -34,8 +34,8 @@
     sessionVariables = {
       MY_HM_VAR = "my-hm-val";
       EDITOR = "nvim";
-      # G_TOKEN=$(cat /$XDG_RUNTIME_DIR/secrets/gh_token)
-      G_TOKEN = "$(cat /run/secrets/gh_token)";
+      # /$XDG_RUNTIME_DIR/secrets/gh_token
+      GH_TOKEN = "$(cat /run/secrets/gh_token)";
     };
   };
 
--- hw/sops.nix
+++ hw/sops.nix
@@ -9,8 +9,7 @@
 
     secrets = {
       gh_token = {
-        # TODO: parameterize
-        owner = config.users.users.iharh.name; 
+        owner = config.users.users.${myuser}.name; 
       };
       "myservice/my_subdir/my_secret" = {
         owner = config.users.users.${myuser}.name;
